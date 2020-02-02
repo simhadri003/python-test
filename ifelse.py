4b478b7990e98aa84368afecf19ae198494f9d42
Sangapu Simhadri <sangapusimhadri003@gmail.com>
2020-02-02 12:18:49 +0530

    Added ifelse file and added types in test file

---
+++ ifelse.py
@@ -0,0 +1,8 @@
+# this file is to the basic conditional statements in python
+# program to check if the input number is even/odd
+
+input_number = int(input("Please enter any number : "))
+if input_number % 2 == 0:
+    print("The given number is even")
+else:
+    print("The given number is odd")
--- test.py
+++ test.py
@@ -1,2 +1,15 @@
 #print hello world to the console
 print("this is simhadri")
+a=5
+print(type(a))
+b=10.5
+print(type(b))
+c='hello'
+print(type(c))
+d=[]
+print(type(d))
+e={}
+print(type(e))
+
+your_name = input("Enter your name")
+print(your_name)
