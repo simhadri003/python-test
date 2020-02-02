5f26f96a345c7cd6367b81c62a30650269093442
Sangapu Simhadri <sangapusimhadri003@gmail.com>
2020-02-02 12:30:51 +0530

    program to check wheather the given number is multiple of five or not

---
+++ five_multiple.py
@@ -0,0 +1,6 @@
+#this program is to check wheater the given number is multiple of five or not
+input_number = int(input("please enter the number"))
+if input_number % 5 == 0:
+    print("the given number is multiple of five")
+else:
+    print("the given number in not a multiple of five")
